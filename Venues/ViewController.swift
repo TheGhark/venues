71880adff1498f5c46950dab89755e7049e7bb3e
Camilo Rodriguez Gaviria <camilo.rodriguez@revolut.com>
2021-04-17 14:33:21 +0200

    Initial Commit

---
+++ Venues/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Venues
+//
+//  Created by Camilo Rodriguez Gaviria on 17/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
