c0e6c8966033505d7f65ddbc3bb5e6c8760de2e4
Camilo Rodriguez Gaviria <camilo.rodriguez@revolut.com>
2021-04-18 14:39:14 +0200

    Add icons

---
+++ Components/Resources/Icon.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+public enum Icon {
+    case camera
+    case error
+    case close
+    case list
+    case offline
+
+    var image: UIImage? {
+        switch self {
+        case .camera:
+            return UIImage(named: "icn_camera")
+        case .error:
+            return UIImage(named: "icn_error")
+        case .close:
+            return UIImage(named: "icn_close")
+        case .list:
+            return UIImage(named: "icn_list")
+        case .offline:
+            return UIImage(named: "icn_offline")
+        }
+    }
+}
