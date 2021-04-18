3ee9717c6b1e5354dfee88428830d359dd70d3a2
Camilo Rodriguez Gaviria <camilo.rodriguez@revolut.com>
2021-04-18 12:47:24 +0200

    Add colors

---
+++ Components/Resources/Color.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+public enum Color {
+    static let background = UIColor(named: "Background")
+    static let foreground = UIColor(named: "Foreground")
+    static let greyBackground = UIColor(named: "GreyBackground")
+    static let greyForeground = UIColor(named: "GreyForeground")
+    static let grey = UIColor(named: "Grey")
+    static let lightGrey = UIColor(named: "LightGrey")
+    static let navigationBar = UIColor(named: "NavigationBar")
+    static let orange = UIColor(named: "Orange")
+    static let translucentBlack72 = UIColor(named: "TranslucentBlack72")
+    static let translucentWhite72 = UIColor(named: "TranslucentWhite72")
+}
